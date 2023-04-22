150a7dfa7f32918f28440bdc0d7ad43e9c1a1193
TheWisePigeon <zozozozeph@gmail.com>
2023-04-22 19:20:59 +0000

    ref: added gitignore

--- nut.sh/nut_manager.sh
+++ nut.sh/nut_manager.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # This is the nutsh manager
 # Responsible of updating/ downloading addons...
+VERSION=""
 
-
-function nutsh (){
-    COMMAND=$1
-    OS=$(uname)
-
+function nut (){
+   COMMAND=$1
+   OS=$(uname)
     case "$1" in
         "version") echo "nutsh v-0.0.1"
         ;;
